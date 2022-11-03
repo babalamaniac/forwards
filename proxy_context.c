7f9e685952ea1a3d0bb4359eede4ba10d8fb77a8
zhuhangchen <zhc426842@qq.com>
2022-11-03 15:25:45 +0800

    improve: use TIOCOUTQ

--- proxy_context.c
+++ proxy_context.c
@@ -2,47 +2,40 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
-#include <fcntl.h>
 
 #include "event_loop.c"
 
 #define READ_CLOSED (1 << 0)
 #define WRITE_CLOSED (1 << 1)
 
+socklen_t optlen = sizeof (int);
+
 struct proxy_context {
     struct event_context * peer_context;
-    int out_buf;
-    int in_buf;
-    int pipe[2];
     int state;
 };
 
-// copy between fd
-ssize_t copy(int from, int to, int type) {
-    int size;
-    int buf = from;
-    if (ioctl(buf, FIONREAD, &size) == -1) {
-        printf("get readable size error, errno=%d, errmsg=%s\n", errno, strerror(errno));
-        size = 0;
+size_t move(int from, int to) {
+    int send_buf_size, send_buf_used, unread_size, size, send_buf_remain;
+    ioctl(to, TIOCOUTQ, &send_buf_used);
+    getsockopt(to, SOL_SOCKET, SO_SNDBUF, &send_buf_size, &optlen);
+    ioctl(from, FIONREAD, &unread_size);
+    send_buf_remain = send_buf_size - send_buf_used;
+    size = send_buf_remain < unread_size ? send_buf_remain : unread_size;
+    if (size <= 0) {
+        return -1;
     }
-    ssize_t count;
-    if (type) {
-        int arr[size + 1];
-        count = read(buf, arr, size + 1);
-        if (count <= 0) {
-            return count;
-        }
-        for (int i = 0; i < count; i++) arr[i] = ~(arr[i]);
-        count = write(to, arr, count);
-    } else {
-        count = splice(buf, 0, to, 0, size + 1, SPLICE_F_NONBLOCK);
-    }
-    return count;
+    printf("send_buf_size=%d, send_buf_used=%d, unread_size=%d, size=%d, send_buf_remain=%d\n", send_buf_size, send_buf_used, unread_size, size, send_buf_remain);
+
+    unsigned char buf[size];
+    ssize_t read_size = read(from, buf, size);
+    for (int i = 0; i < read_size; ++i) buf[i] = ~buf[i];
+    ssize_t write_size = write(to, buf, read_size);
+    printf("expect size = %d, actual read_size = %ld, actual write size = %ld\n", size, read_size, write_size);
 }
 
 void proxy_handle_out(struct event_context * event_context) {
@@ -51,11 +44,7 @@ void proxy_handle_out(struct event_context * event_context) {
         printf("write channel closed, dst fd=%d src fd=%d\n", proxy_context -> peer_context -> fd, event_context -> fd);
         return;
     }
-    ssize_t count = copy(proxy_context -> out_buf, event_context -> fd, 0);
-    if (count == 0) {
-        proxy_context -> state |= WRITE_CLOSED;
-        shutdown(event_context -> fd, SHUT_WR);
-    }
+    move(proxy_context -> peer_context -> fd, event_context -> fd);
 }
 
 void proxy_handle_in(struct event_context * event_context) {
@@ -64,8 +53,7 @@ void proxy_handle_in(struct event_context * event_context) {
         printf("read channel closed, dst fd=%d src fd=%d\n", event_context -> fd, proxy_context -> peer_context -> fd);
         return;
     }
-    copy(event_context -> fd, proxy_context -> in_buf, 1);
-    proxy_handle_out(proxy_context -> peer_context);
+    move(event_context -> fd, proxy_context -> peer_context -> fd);
 }
 
 void do_read_close(struct event_context * event_context) {
@@ -74,7 +62,7 @@ void do_read_close(struct event_context * event_context) {
         return;
     }
     context -> state |= READ_CLOSED;
-    close(context -> in_buf);
+    shutdown(context -> peer_context -> fd, SHUT_WR);
     proxy_handle_out(context -> peer_context);
 }
 
@@ -85,19 +73,13 @@ void do_close(struct event_context * event_context) {
         return;
     }
     context -> state |= WRITE_CLOSED;
-    close(context -> out_buf);
+    shutdown(event_context -> fd, SHUT_WR);
 }
 
 struct proxy_context * init_proxy_context() {
     struct proxy_context *proxy_context = malloc(sizeof(struct proxy_context));
     proxy_context -> state = 0;
     proxy_context -> peer_context = NULL;
-    if (pipe2(proxy_context -> pipe, O_NONBLOCK) == -1) {
-        printf("create pipe error, errno=%d, errmsg=%s\n", errno, strerror(errno));
-    }
-    printf("create pipe, 0=%d, 1=%d\n", proxy_context -> pipe[0], proxy_context -> pipe[1]);
-    proxy_context -> in_buf = (proxy_context -> pipe)[1];
-    fcntl((proxy_context -> pipe)[1], F_SETPIPE_SZ, 100 * 1024 * 1024);
     return proxy_context;
 }
 
@@ -106,8 +88,6 @@ void bind_context(struct event_context * a, struct event_context * b) {
     struct proxy_context * proxy_context_b = get_ext(b);
     proxy_context_a -> peer_context = b;
     proxy_context_b -> peer_context = a;
-    proxy_context_a -> out_buf = proxy_context_b -> pipe[0];
-    proxy_context_b -> out_buf = proxy_context_a -> pipe[0];
 }
 
 struct proxy_init_context {
@@ -141,12 +121,6 @@ struct proxy_context * init_proxy_event_context(struct event_context * event_con
     struct proxy_context * proxy_context = get_ext(event_context);
     proxy_context -> state = 0;
     proxy_context -> peer_context = NULL;
-    if (pipe2(proxy_context -> pipe, O_NONBLOCK) == -1) {
-        printf("create pipe error, errno=%d, errmsg=%s\n", errno, strerror(errno));
-    }
-    printf("create pipe, 0=%d, 1=%d\n", proxy_context -> pipe[0], proxy_context -> pipe[1]);
-    proxy_context -> in_buf = (proxy_context -> pipe)[1];
-    fcntl((proxy_context -> pipe)[1], F_SETPIPE_SZ, 100 * 1024 * 1024);
     return proxy_context;
 }
 
