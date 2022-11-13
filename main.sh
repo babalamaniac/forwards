7c391168f618e466d5bdc148b2c88f23e382e9fd
accelerator <zhc426842@qq.com>
2022-11-13 10:28:40 +0000

    clean

--- main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc main.c -o main
-strace -o main-trace.log ./main 192.168.107.1 33445 192.168.107.6 33445
--- proxy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc proxy.c -o proxy
-strace -o proxy-trace.log ./proxy 192.168.107.1 33445
